1341386d8855e04824c4f035d2dd672a3cb48177
cedricbour <cedric.bour@live.fr>
2021-02-09 22:43:53 +0100

    fix calcul volume

--- binance.js
+++ binance.js
@@ -123,26 +123,26 @@ binance.websockets.bookTickers(undefined, (callback) => {
                         let minPrice = (info.filter(val => val['filterType'] === 'PRICE_FILTER'))[0]
 
                         let lenVol = minVolume.minQty.split('.')
-                        lenVol = lenVol.length > 1 ? (lenVol[1].split('1')[0] + '1').length : 0
+                        lenVol = lenVol[0] === "0" ? (lenVol[1].split('1')[0] + '1').length : 0
                         let volume = String(mise / value.price)
                         volume = volume.substr(0, volume.split('.')[0].length + (lenVol ? 1 : 0) + lenVol)
 
                         let lenPrice = minPrice.minPrice.split('.')
-                        lenPrice = lenPrice.length > 1 ? (lenPrice[1].split('1')[0] + '1').length : 0
+                        lenPrice = lenPrice[0] === "0" ? (lenPrice[1].split('1')[0] + '1').length : 0
                         let price = String((Number(value.price) * profit / 100) + Number(value.price))
                         price = price.substr(0, price.split('.')[0].length + (lenPrice ? 1 : 0) + lenPrice)
 
                         await binance.marketBuy(value.symbol, volume, (error,) => {
                             if (error !== null) {
                                 let responseJson = JSON.parse(error.body)
-                                console.log(value.symbol + " [" + responseJson.code + "]: " + responseJson["msg"] + " " + price + " " + volume)
+                                console.error(value.symbol + " [" + responseJson.code + "]: " + responseJson["msg"] + " " + price + " " + volume)
                             } else {
                                 console.log(value.symbol + " buy")
                                 balances["USDT"].available -= mise
                                 binance.sell(value.symbol, volume, price, {type: 'LIMIT'}, (error,) => {
                                     if (error !== null) {
                                         let responseJson = JSON.parse(error.body)
-                                        console.log(value.symbol + " [" + responseJson.code + "]: " + responseJson["msg"] + " " + price + " " + volume)
+                                        console.error(value.symbol + " [" + responseJson.code + "]: " + responseJson["msg"] + " " + price + " " + volume)
                                     } else {
                                         console.log(value.symbol + " sell")
                                         new_orders.push(order(
