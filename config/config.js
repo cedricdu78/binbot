0e50ce60b5164d4e558878dec1cc6bc5df5dc0d9
cedric-bour <cedric.bour@live.fr>
2021-03-24 15:04:09 +0100

    to english

--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
 module.exports = {
 
-    // keep_balance :   Combien on garde d'argent de côté.
-    // noOrder :        Retourne les cryptos qui n'ont pas d'ordre et dont la valeurs est supérieur ou égale à 1
-    // marketPrc :      Le marché est baissié quand le pourentage de crypto achetable est supérieur à 20%
-    // onlyShort :      Le bot n'achète que quand le marché est detecter comme baissier
+    // keep_balance :   How much we keep the money.
+    // noOrder :        Returns cryptos that have no order and whose value is greater than or equal to 1
+    // marketPrc :      The market is bearish when the percentage of buyable crypto is greater than 20%
+    // onlyShort :      The bot only buys when the market is detected as bearish
     keep_balance: function () { return 0 },
     noOrder: function () { return 1 },
     marketPrc: function () { return 10 },
     onlyShort: function () { return false },
 
-    // mise :       La mise est de 4% de votre solde total (exemple pour 4000$ de solde spot, mise de 160$)
-    // profit :     Pourcentage de gain attendu par achat sur une crypto
-    // interval :   Historique de la crypto toutes les 15mins sur une semaine
-    // median :     Achat d'une crypto si prix compris entre -0% et -20% de la moy calculé sur une semaine
-    // prc :        Pourcentage de différence entre la moyenne et le maximum sur la semaine
+    // mise :       The stake is 4% of your total balance (example for $4000 spot balance, stake of $160)
+    // profit :     Percentage of expected gain per purchase on a crypto
+    // interval :   Crypto history every 15 minutes over a week
+    // median :     Purchase of a crypto if price between -0% and -20% of the average calculated over a week
+    // prc :        Percentage difference between the average and the maximum over the week
     mise: function () { return 4 },
     profit: function () { return 10 },
     interval: function () { return ['15m', 673] },
     median: function () { return [5, 20] },
     prc: function () { return 10 },
 
-    // baseMoney    : USDT is base for trading crypto (is stablecoin)
-    // baseSymbol   : Symbol de la monney utilisé
-    // feeMoney     : Use BNB for payement fee trading because -25%
+    // baseMoney    : base for crypto trading !choose stable coin!
+    // baseSymbol   : Currency symbol used
+    // feeMoney     : Use BNB for payment fee trading because -25%
     // feeValue     : Fee value with BNB is 0.750 but for security 0.15
     baseMoney: function () { return "USDT" },
     baseSymbol: function () { return "$" },
@@ -31,7 +31,7 @@ module.exports = {
 
     // colResume :  Column name in console output
     // colCrypto :  Column name in console output
-    // refresh :    On attends 20s avant de relancer le bot (en ms)
+    // refresh :    We wait 20s before relaunching the bot (in ms)
     colResume: function () { return ["Placed", "Current", "Target"] },
     colCrypto: function () { return ["Currencies", "Available", "Total", "Rapport", "Marché", "baissier", "haussier"] },
     refresh: function () { return 20000 },
