1ef6b1ccb89dec0a4f713ba756541f9ac5c08531
cedric-bour <cedric.bour@live.fr>
2021-03-25 19:26:41 +0100

    optimize

--- binance.js
+++ binance.js
@@ -12,87 +12,81 @@ const binance = new Binance().options({
 function getBalances() {
     binance.balance().then(balances => {
         getOpenOrders(balances)
-    }).catch(error => {
-        console.error(error)
-    }).finally(() => new Promise(res => setTimeout(res, config.refresh())).finally(() => getBalances()))
+    }).catch(error => console.error(error)
+    ).finally(() => new Promise(res => setTimeout(res, config.refresh())).finally(() => getBalances()))
 }
 
 // get open order
 function getOpenOrders(balances) {
     binance.openOrders().then(openOrders => {
         getCurrencies(balances, openOrders)
-    }).catch(error => {
-        console.error(error)
-    })
+    }).catch(error => console.error(error))
 }
 
 // get currencies available
 function getCurrencies(balances, openOrders) {
     binance.exchangeInfo().then(exchangeInfo => {
-        getHistories(Object.entries(exchangeInfo['symbols']).filter(([, value]) =>
-            value.symbol.endsWith(config.baseMoney())
-            && !value.symbol.endsWith('DOWN' + config.baseMoney())
-            && !value.symbol.endsWith('UP' + config.baseMoney())
-            && !value.symbol.endsWith('BULL' + config.baseMoney())
-            && !value.symbol.endsWith('BEAR' + config.baseMoney())
-            && value.status !== 'BREAK'), balances, openOrders)
+        let total = Number(balances[config.baseMoney()].available) + Number(balances[config.baseMoney()].onOrder),
+            counter = 0, currencies = []
+
+        Object.entries(exchangeInfo['symbols']).forEach(([, value]) => {
+            if (value.symbol.endsWith(config.baseMoney())
+                && !value.symbol.endsWith('DOWN' + config.baseMoney())
+                && !value.symbol.endsWith('UP' + config.baseMoney())
+                && !value.symbol.endsWith('BULL' + config.baseMoney())
+                && !value.symbol.endsWith('BEAR' + config.baseMoney())
+                && value.status !== 'BREAK') {
+
+                getHistories(value).then(value => {
+                    total += getTotal(value, balances)
+                    getNoOrders(value, balances)
+                    currencies.push(value)
+
+                    if (++counter === exchangeInfo['symbols'].length)
+                        prepareBuy(currencies, balances, openOrders, total)
+                }).catch(error => console.error(error + " " + value.symbol + " " + value.price))
+            } else if (++counter === exchangeInfo['symbols'].length)
+                prepareBuy(currencies, balances, openOrders, total)
+        })
+    }).catch(error => console.error(error))
+}
+
+// get history per currency
+function getHistories(value) {
+    return binance.candlesticks(value.symbol, config.interval()[0], null, {limit: config.interval()[1]}).then(res => {
+        value.moy = []
+        res.forEach(function (val) {
+            value.moy.push(Number(val[4]))
+        })
+        value.price = value.moy[value.moy.length - 1]
+
+        let minPrice = (value['filters'].filter(val => val['filterType'] === 'PRICE_FILTER'))[0]
+        let minVolume = (value['filters'].filter(val => val['filterType'] === 'LOT_SIZE'))[0]
+        value.lenPrice = minPrice.minPrice.split('.')[0] === "0"
+            ? (minPrice.minPrice.split('.')[1].split('1')[0] + '1').length : 0
+        value.lenVol = minVolume.minQty.split('.')[0] === "0"
+            ? (minVolume.minQty.split('.')[1].split('1')[0] + '1').length : 0
+
+        return value
     }).catch(error => {
         console.error(error)
     })
 }
 
-// get history per currency
-function getHistories(currencies, balances, openOrders) {
-    try {
-        let counter = 0
-        Object.entries(currencies).forEach(function ([, [, value]]) {
-            binance.candlesticks(value.symbol, config.interval()[0], (error, res) => {
-                if (error !== null) new Error(error);
-                else {
-                    value.moy = []
-                    res.forEach(function (val) {
-                        value.moy.push(Number(val[4]))
-                    })
-                    value.price = value.moy[value.moy.length - 1]
-
-                    let minPrice = (value['filters'].filter(val => val['filterType'] === 'PRICE_FILTER'))[0]
-                    let minVolume = (value['filters'].filter(val => val['filterType'] === 'LOT_SIZE'))[0]
-                    value.lenPrice = minPrice.minPrice.split('.')[0] === "0"
-                        ? (minPrice.minPrice.split('.')[1].split('1')[0] + '1').length : 0
-                    value.lenVol = minVolume.minQty.split('.')[0] === "0"
-                        ? (minVolume.minQty.split('.')[1].split('1')[0] + '1').length : 0
-
-                    if (++counter === currencies.length) getNoOrders(balances, currencies, openOrders)
-                }
-            }, {limit: config.interval()[1]})
-        })
-    } catch (err) {
-        console.error(err)
-    }
+function getTotal(value, balances) {
+    return value.price * Number(balances[value['baseAsset']].available)
+        + value.price * Number(balances[value['baseAsset']].onOrder)
 }
 
 // get currency without order
-function getNoOrders(balances, currencies, openOrders) {
-    try {
-        let counter = 0, total = 0
-        Object.entries(currencies).forEach(function ([, [, value]]) {
-            if (balances[value['baseAsset']].available * value.price >= config.noOrder()
-                && value['baseAsset'] !== config.feeMoney())
-                console.error(value.symbol + ' has units out of order: '
-                    + (balances[value['baseAsset']].available * value.price) + config.baseSymbol())
+function getNoOrders(value, balances) {
+    if (balances[value['baseAsset']].available * value.price >= config.noOrder()
+        && value['baseAsset'] !== config.feeMoney())
+        console.error(value.symbol + ' has units out of order: '
+            + (balances[value['baseAsset']].available * value.price) + config.baseSymbol())
 
-            total += value.price * Number(balances[value['baseAsset']].available)
-            total += value.price * Number(balances[value['baseAsset']].onOrder)
-
-            if (value.symbol === config.feeMoney() + config.baseMoney())
-                balances[config.feeMoney()].available *= value.price
-
-            if (++counter === currencies.length)
-                prepareBuy(currencies, balances, openOrders, total)
-        })
-    } catch (err) {
-        console.error(err)
-    }
+    if (value.symbol === config.feeMoney() + config.baseMoney())
+        balances[config.feeMoney()].available *= value.price
 }
 
 // buy currency
@@ -162,15 +156,11 @@ function buyLimit(currencies, curr, new_orders, total, details, balances, orders
 // Remove cryptocurrencies that do not match the purchase condition
 function prepareBuy(currencies, balances, openOrders, total) {
     try {
-
-        total += Number(balances[config.baseMoney()].available)
-        total += Number(balances[config.baseMoney()].onOrder)
-
         let curr = [], details = [], new_orders = [], orders = []
         let counter = 0, open = 0, now = 0, want = 0, mise = total * config.mise() / 100;
 
         Object.entries(openOrders).forEach(function ([, value]) {
-            let curr = Object.entries(currencies).filter(([, [, val]]) => val.symbol === value.symbol)[0][1][1]
+            let curr = Object.entries(currencies).filter(([, val]) => val.symbol === value.symbol)[0][1]
 
             let volume = String(value['origQty'])
             volume = volume.substr(0, volume.split('.')[0].length
@@ -203,7 +193,7 @@ function prepareBuy(currencies, balances, openOrders, total) {
             ))
         })
 
-        Object.entries(currencies).forEach(function ([, [, value]]) {
+        Object.entries(currencies).forEach(function ([, value]) {
 
             if (Number(balances[value['baseAsset']].onOrder) === 0
                 && Number(balances[value['baseAsset']].available) === 0) {
@@ -223,13 +213,13 @@ function prepareBuy(currencies, balances, openOrders, total) {
                 if (value.moy * (100 - config.median()[1]) / 100 > value.price
                     || value.moy * (100 - config.median()[0]) / 100 < value.price
                     || value.price <= 0 || prc < 10)
-                    curr = Object.entries(currencies).filter(([, [, val]]) => val.symbol !== value.symbol)
+                    curr = Object.entries(currencies).filter(([, val]) => val.symbol !== value.symbol)
             }
 
             if (++counter === currencies.length) {
                 let curr2 = []
-                curr = curr.filter(([, [, val]]) => val.amprice <= 0)
-                Object.entries(curr).forEach(([, [, [, v]]]) => {
+                curr = curr.filter(([, val]) => val.amprice <= 0)
+                Object.entries(curr).forEach(([, v]) => {
                     curr2.push(v)
                 })
 
