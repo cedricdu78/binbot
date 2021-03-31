3088919e8a87108f4f4387dfa0084ee645ae639d
cedric-bour <cedric.bour@live.fr>
2021-03-31 22:58:17 +0200

    fix

--- binance.js
+++ binance.js
@@ -270,7 +270,7 @@ class Bot {
 
     getConsole() {
         if (this.orders.length > 0) console.table(this.orders.sort((a, b) => b.plusValue - a.plusValue))
-        if (this.resume.details.length > 0) console.table(this.resume.details, ["symbol", "am_price"])
+        if (this.exchangeInfo.length > 0) console.table(this.resume.details, ["symbol", "am_price"])
         if (this.newOrders.length > 0) console.table(this.newOrders)
         if (this.unordered.length > 0) console.table(this.unordered)
         console.table({
